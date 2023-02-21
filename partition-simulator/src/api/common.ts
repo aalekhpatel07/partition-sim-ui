baf3383394de80b06c6660aabd8f0018b93fa8ff
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2023-02-20 22:07:44 -0600

    Refactor the api base url to .env.* files
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- partition-simulator/src/api/common.ts
+++ partition-simulator/src/api/common.ts
@@ -1,31 +1,27 @@
-const API_BASE_URL = 'http://localhost:3000/api/v1';
-
+const API_BASE_URL = import.meta.env.PUBLIC_PARTITION_SIM_API_URL;
 
 export async function get<O, E = string>(endpoint: string): Promise<O> {
-    return fetch(
-        `${API_BASE_URL}/${endpoint}`,
-        {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        }
-    )
-    .then(response => response.json())
-    .catch(err => err as E);
+  return fetch(`${API_BASE_URL}/${endpoint}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .catch((err) => err as E);
 }
 
-export async function post<I, O, E = string>(endpoint: string, body: I): Promise<O> {
-    return fetch(
-        `${API_BASE_URL}/${endpoint}`,
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(body)
-        }
-    )
-    .then(response => response.json())
-    .catch(err => err as E);
+export async function post<I, O, E = string>(
+  endpoint: string,
+  body: I
+): Promise<O> {
+  return fetch(`${API_BASE_URL}/${endpoint}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then((response) => response.json())
+    .catch((err) => err as E);
 }
